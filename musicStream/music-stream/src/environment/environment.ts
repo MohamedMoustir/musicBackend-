1c3b542e607d54a8bbef90edc21c46a26f14fd40
MohamedMoustir <itsmoustir@gmail.com>
2026-01-30 19:50:59 +0100

    fix: add HttpClientTestingModule to missing specs

--- musicStream/music-stream/src/environment/environment.ts
+++ musicStream/music-stream/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
  production : false,
- apiUrl: 'http://localhost:8080/api/tracks'
-//   apiUrl: 'https://musicbackend-8i9a.onrender.com/api/tracks'
+//  apiUrl: 'http://localhost:8080/api/tracks'
+    apiUrl: 'https://musicbackend-8i9a.onrender.com/api/tracks'
 
 }
