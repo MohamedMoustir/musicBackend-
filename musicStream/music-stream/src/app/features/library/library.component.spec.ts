a5c586bb8c285972f18fc0f7ab61a7f8ec7c7b9f
MohamedMoustir <itsmoustir@gmail.com>
2026-01-30 15:15:28 +0100

    chore(docker): update docker-compose configuration

--- musicStream/music-stream/src/app/features/library/library.component.spec.ts
+++ musicStream/music-stream/src/app/features/library/library.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LibraryComponent } from './library.component';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TrackService } from '../../core/services/track.service';
+import { ActivatedRoute } from '@angular/router';
+import { provideMockStore } from '@ngrx/store/testing';
 describe('LibraryComponent', () => {
   let component: LibraryComponent;
   let fixture: ComponentFixture<LibraryComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LibraryComponent]
+      imports: [LibraryComponent, HttpClientTestingModule],
+      providers: [provideMockStore({}),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {get:()=> null}
+            }
+          }
+        }
+      ]
+
     })
     .compileComponents();
 
