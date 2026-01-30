71e924d1c2510d236c768afa42e1ef3e8e54c45f
MohamedMoustir <itsmoustir@gmail.com>
2026-01-30 11:42:46 +0100

    feat(pipe): add format time pipe

--- musicStream/music-stream/src/app/shared/pipes/format-time.pipe.ts
+++ musicStream/music-stream/src/app/shared/pipes/format-time.pipe.ts
@@ -6,15 +6,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatTimePipe implements PipeTransform {
 
-  transform(timeInSeconds: number): unknown {
-    if (!timeInSeconds || isNaN(timeInSeconds)) return '00:00';
-    const minutes = Math.floor(timeInSeconds / 60);
-    const seconds = Math.floor(timeInSeconds % 60);
+  transform(time: number | undefined | null): unknown {
+    if (!time || isNaN(time)) {
+      return '0:00';
+    }
 
-    const paddingMinutes = minutes < 10 ? '0' : '';
-    const paddingSeconds = seconds < 10 ? '0' : '';
-
-    return `${paddingMinutes}${minutes}:${paddingSeconds}${seconds}`;
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   }
 
 }
