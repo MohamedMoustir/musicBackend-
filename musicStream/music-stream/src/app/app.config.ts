221a19e9796add82173a89a01ab477dae97083ed
MohamedMoustir <itsmoustir@gmail.com>
2026-01-30 11:42:43 +0100

    chore(app): configure NgRx providers

--- musicStream/music-stream/src/app/app.config.ts
+++ musicStream/music-stream/src/app/app.config.ts
@@ -1,9 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './core/store/interceptors/error.interceptor';
+import { provideState, provideStore } from '@ngrx/store';
+import { trackReducer } from './core/store/track.reducer';
+import { provideEffects } from '@ngrx/effects';
+import * as trackEffects from './core/store/track.effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { max } from 'rxjs';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(withFetch())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes),
+     provideHttpClient(withInterceptors([errorInterceptor])),
+     provideStore(),
+     provideState({name:'tracks',reducer:trackReducer}),
+     provideEffects(trackEffects),
+     provideStoreDevtools({maxAge: 25, logOnly: !isDevMode()})
+
+    
+    ]
+
 };
