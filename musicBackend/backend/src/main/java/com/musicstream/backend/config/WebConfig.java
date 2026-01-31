f689241774737a024b8c7ba066ce9b269e51cb78
MohamedMoustir <itsmoustir@gmail.com>
2026-01-31 13:58:36 +0100

    Fix CORS methods and update configuration

--- musicBackend/backend/src/main/java/com/musicstream/backend/config/WebConfig.java
+++ musicBackend/backend/src/main/java/com/musicstream/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200","https://thunderous-peony-6231dc.netlify.app")
+                .allowedOrigins("http://localhost:4200","https://audioflow.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
