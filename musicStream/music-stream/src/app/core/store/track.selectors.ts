cbfcd914957c56ccc756b9daffe45d03584994be
MohamedMoustir <itsmoustir@gmail.com>
2026-01-31 14:09:17 +0100

    Allow all origins for CORS to fix 403 error

--- musicStream/music-stream/src/app/core/store/track.selectors.ts
+++ musicStream/music-stream/src/app/core/store/track.selectors.ts
@@ -1,8 +1,6 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { TrackState } from "./track.reducer";
 
-
-
 export const selectTrackState = createFeatureSelector<TrackState>('tracks');
 
 export const selectAllTracks = createSelector(
