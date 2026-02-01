6b6d41b6af2d53cb676bbb4bc2daf435885a8fc8
MohamedMoustir <itsmoustir@gmail.com>
2026-02-01 17:34:05 +0100

    refactor(store): update track effects for optimized data handling

--- musicStream/music-stream/src/app/core/store/track.effects.ts
+++ musicStream/music-stream/src/app/core/store/track.effects.ts
@@ -2,37 +2,16 @@ import { inject } from "@angular/core";
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as TrackActions from "./track.actions";
 import { TrackService } from "../services/track.service";
-import { catchError, map, mergeMap, of } from "rxjs";
+import { catchError, map, switchMap, concatMap, tap, of } from "rxjs"; // ✅ زدنا switchMap و concatMap و tap
 import { Router } from "@angular/router";
 
 export const loadTracksEffect = createEffect(
-    (action$ = inject(Actions), trackService = inject(TrackService), router = inject(Router)) => {
+    (action$ = inject(Actions), trackService = inject(TrackService)) => {
         return action$.pipe(
             ofType(TrackActions.loadTracks),
-            mergeMap(() =>
+            switchMap(() =>
                 trackService.getAllTracks().pipe(
-                    map(tracks => {
-                        router.navigate(['/library'])
-                        return TrackActions.loadTracksSuccess({ tracks });
-
-                    }),
-                    catchError(error => of(TrackActions.loadTracksFailure({ error: error.message })))
-                ))
-        );
-    },
-    { functional: true }
-
-);
-export const addTrackEffect = createEffect(
-    (actions$ = inject(Actions), trackService = inject(TrackService), router = inject(Router)) => {
-        return actions$.pipe(
-            ofType(TrackActions.addTrack),
-            mergeMap(({ file, metadata }) =>
-                trackService.createTrack(file, metadata).pipe(
-                    map(track => {
-                        router.navigate(['/library']);
-                        return TrackActions.addTrackSuccess({ track });
-                    }),
+                    map(tracks => TrackActions.loadTracksSuccess({ tracks })),
                     catchError(error => of(TrackActions.loadTracksFailure({ error: error.message })))
                 )
             )
@@ -40,28 +19,46 @@ export const addTrackEffect = createEffect(
     },
     { functional: true }
 );
+
+export const addTrackEffect = createEffect(
+    (actions$ = inject(Actions), trackService = inject(TrackService), router = inject(Router)) => {
+        return actions$.pipe(
+            ofType(TrackActions.addTrack),
+            concatMap(({ file, metadata }) =>
+                trackService.createTrack(file, metadata).pipe(
+                    tap(() => router.navigate(['/library'])), 
+                    map(track => TrackActions.addTrackSuccess({ track })),
+                    catchError(error => of(TrackActions.loadTracksFailure({ error: error.message })))
+                )
+            )
+        );
+    },
+    { functional: true }
+);
+
+
+
 export const updateTrackEffect = createEffect(
     (action$ = inject(Actions), trackService = inject(TrackService), router = inject(Router)) => {
         return action$.pipe(
             ofType(TrackActions.updateTrack),
-            mergeMap(({ trackId, formData }) =>
+            concatMap(({ trackId, formData }) =>
                 trackService.updateTrack(trackId, formData).pipe(
-                    map((updatedTrack) => {
-                        router.navigate(['/library'])
-                        return TrackActions.updateTrackSuccess({ track: updatedTrack });
-                    }),
+                    tap(() => router.navigate(['/library'])), 
+                    map((updatedTrack) => TrackActions.updateTrackSuccess({ track: updatedTrack })),
                     catchError((error) => of(TrackActions.loadTracksFailure({ error: error.message })))
                 )
             )
         );
     },
     { functional: true }
-)
+);
+
 export const deleteTrackEffect = createEffect(
   (actions$ = inject(Actions), trackService = inject(TrackService)) => {
     return actions$.pipe(
       ofType(TrackActions.deleteTrack),
-      mergeMap(({ id }) =>
+      concatMap(({ id }) =>
         trackService.deleteTrack(id).pipe(
           map(() => TrackActions.deleteTrackSuccess({ id })),
           catchError((error) => of(TrackActions.loadTracksFailure({ error: error.message })))
